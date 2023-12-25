fc85cff1efeee2e618e7b848d016c35a554151c9
Indra Wahjoedi <iwahjoedi@users.noreply.github.com>
2023-12-25 19:34:00 +0000

    fix vhost

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,8 @@ REPO_FOLDER="/workspaces/$RepositoryName"
 
 # Apache
 sudo chmod 777 /etc/apache2/sites-available/000-default.conf
-sudo sed "s@.*ServerName.*@\tServerName $(CODESPACE_NAME)" .devcontainer/000-default.conf
 sudo sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-default.conf > /etc/apache2/sites-available/000-default.conf
+sudo sed -i -r "s@.*ServerName.*@\tServerName $CODESPACE_NAME@" /etc/apache2/sites-available/000-default.conf
 update-rc.d apache2 defaults 
 service apache2 start
 
